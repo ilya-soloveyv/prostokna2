ca55c6b42941282fd9bc3d185d0baa80f73c6f11
ilya <ilya.soloveyv@gmail.com>
2019-07-04 00:38:29 +0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="container">
-    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
     <h1 class="title">
       USERS
     </h1>
@@ -29,20 +28,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.title
-{
-  margin: 30px 0;
-}
-.users
-{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.user
-{
-  margin: 10px 0;
-}
-</style>
