05415a1830d61444558f3790568889f5fe24d822
ilya <ilya.soloveyv@gmail.com>
2019-07-04 04:00:43 +0300

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
-  div
-    nuxt
+  .wrapper
+    header
+    nav
+    main
+      nuxt
 </template>
 
 <script>
@@ -10,5 +13,34 @@ export default {
 </script>
 
 <style lang="scss">
-
+html, body {
+  margin: 0;
+  padding: 0;
+}
+.wrapper {
+  height: 100vh;
+  display: grid;
+  grid-gap: 1px;
+  grid-template-columns: 60px 1fr;
+  grid-template-rows: 60px 1fr;
+  grid-template-areas: "header header" "nav main";
+  overflow: hidden;
+  @media (max-width: 500px) {
+    grid-template-columns: 1fr;
+    grid-template-areas: "header header" "main";
+  }
+}
+header {
+  grid-area: header;
+  background: #CCCCCC;
+}
+main {
+  grid-area: main;
+  background: #CCCCCC;
+  overflow: auto;
+}
+nav {
+  grid-area: nav;
+  background: #CCCCCC;
+}
 </style>
